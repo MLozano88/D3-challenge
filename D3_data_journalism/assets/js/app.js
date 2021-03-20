7b784db55c791979fe0a5a00ace1105b1a6d4941
Mario Lozano <m.lozano1733@gmail.com>
2021-03-20 10:54:50 -0400

    update script

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -22,7 +22,25 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Importing data
-d3.csv("data.csv").then(function(data) {
+d3.csv("assests/data/data.csv").then(function(data) {
 
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    data.forEach(function(info) {
+        info.smokes = +info.smokes;
+        info.age = +info.age;
+      });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(data, d => d.smokes)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.age)])
+      .range([height, 0]);
+
+    
 
   });
